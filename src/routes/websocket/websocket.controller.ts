f83bc912071ab570f1bc49d3e986fadbcac3c09e
MartinKamenov <martinkamenov10@gmail.com>
2019-10-27 11:46:29 +0200

    Add websocket functionality to controller

--- src/routes/websocket/websocket.controller.ts
+++ src/routes/websocket/websocket.controller.ts
@@ -1,6 +1,26 @@
-const controller = {
-    addConnection: (connection) => {
+const connections = {};
 
+const controller = {
+    addConnection: (id: string, connection: any) => {
+        if(connections[id]) {
+            connections[id] = [];
+        }
+
+        connections[id].push(connection);
+    },
+    sendMessage: (id: string, message: string) => {
+        const allConnections = connections[id];
+        if(!allConnections) {
+            return;
+        }
+
+        allConnections
+            .forEach(connection => (
+                connection.send(JSON.stringify(message)))
+            );
+    },
+    removeConnection: (id: string) => {
+        delete connections[id];
     }
 };
 
--- src/routes/websocket/websocket.route.ts
+++ src/routes/websocket/websocket.route.ts
@@ -1,6 +1,7 @@
 import controller from './websocket.controller';
 import Route from '../contracts/Route';
 const { Router } = require('express');
+const enableWs = require('express-ws');
 
 export default class WebsocketRoute implements Route {
     constructor(private app) {}
@@ -8,10 +9,25 @@ export default class WebsocketRoute implements Route {
     attach = () => {
         const router = new Router();
 
+        enableWs(this.app);
+
+        this.app.ws('/:id', async (ws, req) => {
+            const id = req.params.id;
+
+            controller.addConnection(id, ws);
+            ws.on('message', async (msg) => {
+                controller.sendMessage(id, msg);
+            });
+
+            ws.on('close', () => {
+                controller.removeConnection(id);
+            });
+        });
+
         router.get('/', (req, res) => {
             res.send('WebsocketUS');
         });
 
-        this.app.use('/', router);
+        this.app.use('/websockets', router);
     }
 }
